66cc40d3bf9cae9f10062ea5cd8174f16dd7d52a
andytow <andy@towsa.com>
2013-06-28 12:41:12 -0300

    Create bloques.js

---
+++ public/assets/js/bloques.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    $('.chk-bloque-ninguno').hide();
+
+    $('.bloques').on('click', '.chk-bloque', function() {
+        if($('.chk-bloque:checked').length == 0) {
+            $('circle').removeClass('bloque-blur bloque-focus');
+            $('.chk-bloque-ninguno').hide();
+        }
+        else {
+            $('.chk-bloque-ninguno').show();
+            var bloque = $(this).val();
+            if($(this).is(':checked')) {
+                $('.bloque' + bloque).addClass('bloque-focus');
+                $('circle').not('.bloque' + bloque + ' bloque-focus').addClass('bloque-blur');
+            }
+            else
+                $('.bloque' + bloque).addClass('bloque-blur').removeClass('bloque-focus');
+        }
+    });
+
+    $('.bloques').on('click', '.chk-bloque-ninguno', function() {
+        $('circle').removeClass('bloque-blur bloque-focus');
+        $('.chk-bloque:checked').removeAttr('checked');
+        $(this).hide();
+    });
+});
+
+var blocksChecked;
+function getCheckedBlocks() {
+    blocksChecked = [];
+    $('.chk-bloque:checked').each(function() {
+        blocksChecked.push($(this).val());
+    });
+}
+function setCheckedBlocks() {
+    for(var v in blocksChecked)
+        $('.chk-bloque[value="' + blocksChecked[v] + '"]').attr('checked', 'checked');
+}
